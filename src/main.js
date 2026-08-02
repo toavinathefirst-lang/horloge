672b39a208911d89ceb7e2e1b478a0096571edbd
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-08-02 13:28:21 +0300

    parti des seconde fini

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 
+const secondsHand = document.getElementById("seconds_hand");
+const minutesHand = document.getElementById("minutes_hand");
+const hoursHand = document.getElementById("hours_hand");
+
+console.log(secondsHand);
+
+function getTime(){
+    const now =new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const timeInterval=6
+    
+    secondsHand.style.transform = "rotate("+ (seconds*timeInterval)+"deg)"
+}
+
+setInterval(getTime,100)
